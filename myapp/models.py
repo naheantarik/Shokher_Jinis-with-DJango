73160a23c87014c56dde01576a3de35cdfece557
naheantarik <nahean.swe@gmail.com>
2023-03-31 16:29:55 +0600

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -12,3 +12,10 @@ class Product(models.Model):
         return self.name
     
 
+class Member(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=11, null=True)
+
+    def __str__(self):
+        return self.name
+
