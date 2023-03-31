bc8489db8cdd535e27b3fb74f7c669f3dcfa8599
naheantarik <nahean.swe@gmail.com>
2023-03-31 16:07:01 +0600

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    price = models.IntegerField(default=0, null=True)
+    description = models.CharField(default='', max_length=100)
+    image = models.ImageField('media/product')
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
