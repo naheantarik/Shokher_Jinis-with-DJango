58beeb740ff38c15e37e08b49ba66d3b89f47f48
naheantarik <nahean.swe@gmail.com>
2023-03-31 16:48:13 +0600

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     price = models.IntegerField(default=0, null=True)
-    description = models.CharField(default='', max_length=100)
+    description = models.CharField(max_length=100, blank=True)
     date_create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Product(models.Model):
 
 class Member(models.Model):
     name = models.CharField(max_length=50, null=True)
-    phone = models.CharField(max_length=11, null=True)
+    phone = models.CharField(default='+880', max_length=14, null=True)
 
     def __str__(self):
         return self.name
