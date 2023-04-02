3c51938c0e7b9494e6fd6614b5270b61e47829ec
naheantarik <nahean.swe@gmail.com>
2023-04-02 23:21:42 +0600

    Initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'dashboard.html')
+    product = Product.get_all_product()
+
+
+    return render(request, 'dashboard.html', {'product': product})
